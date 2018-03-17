9ee8968858cc130ecf869d1c3bf3b8aebf97fa99
Matt Glaman <nmd.matt@gmail.com>
2018-03-17 01:02:01 -0500

    Domain support

---
+++ src/Plugin/Commerce/StoreResolverMethod/Domain.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\commerce_store_resolver\Plugin\Commerce\StoreResolverMethod;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @StoreResolverMethod(
+ *   id = "domain",
+ *   label = "Domain",
+ * )
+ *
+ * Deprecates module `commerce_country_domain`
+ */
+class Domain extends StoreResolverMethodBase {
+
+  protected $requestStack;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, RequestStack $request_stack) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager);
+    $this->requestStack = $request_stack;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolve() {
+    $current_request = $this->requestStack->getCurrentRequest();
+    $current_host = $current_request->getHost();
+    $query = $this->storeStorage->getQuery();
+    $query->condition('domain', $current_host);
+    $store_ids = $query->execute();
+    if (!empty($store_ids)) {
+      $store_id = reset($store_ids);
+      return $this->storeStorage->load($store_id);
+    }
+  }
+
+}
---
+++ tests/src/Kernel/DomainStoreResolverMethodTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\Tests\commerce_store_resolver\Kernel;
+
+use Drupal\commerce_store\Entity\Store;
+use Drupal\commerce_store\Entity\StoreInterface;
+use Drupal\commerce_store\StoreCreationTrait;
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+use Symfony\Component\HttpFoundation\Request;
+
+class DomainStoreResolverMethodTest extends EntityKernelTestBase {
+
+  public static $modules = [
+    'address',
+    'datetime',
+    'entity',
+    'options',
+    'inline_entity_form',
+    'views',
+    'commerce',
+    'commerce_price',
+    'commerce_store',
+    'commerce_store_resolver',
+  ];
+
+  /**
+   * The default store.
+   *
+   * @var \Drupal\commerce_store\Entity\StoreInterface
+   */
+  protected $store1;
+
+  /**
+   * The default store.
+   *
+   * @var \Drupal\commerce_store\Entity\StoreInterface
+   */
+  protected $store2;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installSchema('system', 'router');
+    $this->installEntitySchema('commerce_currency');
+    $this->installEntitySchema('commerce_store');
+    $this->installConfig(['commerce_store']);
+
+    $currency_importer = $this->container->get('commerce_price.currency_importer');
+    $currency_importer->import('USD');
+    $currency_importer->import('CAD');
+
+    $store = Store::create([
+      'type' => 'online',
+      'uid' => 1,
+      'name' => 'United States',
+      'mail' => 'admin@example.com',
+      'address' => [
+        'country_code' => 'US',
+        'address_line1' => $this->randomString(),
+        'locality' => $this->randomString(5),
+        'administrative_area' => 'WI',
+        'postal_code' => '53597',
+      ],
+      'default_currency' => 'USD',
+      'billing_countries' => [
+        'US',
+      ],
+      'domain' => 'mystore.com',
+    ]);
+    $store->save();
+    $this->store1 = $store;
+
+    $store = Store::create([
+      'type' => 'online',
+      'uid' => 1,
+      'name' => 'Canada',
+      'mail' => 'admin@example.com',
+      'address' => [
+        'country_code' => 'CA',
+        'address_line1' => $this->randomString(),
+        'locality' => $this->randomString(5),
+        'administrative_area' => 'BC',
+        'postal_code' => ' V1X 6Y5',
+      ],
+      'default_currency' => 'CAD',
+      'billing_countries' => [
+        'CA',
+      ],
+      'domain' => 'ca.mystore.com',
+    ]);
+    $store->save();
+    $this->store2 = $store;
+    $this->container->get('entity_type.manager')->getStorage('commerce_store')->markAsDefault($this->store1);
+  }
+
+  /**
+   * Tests the resolver based on country code header.
+   */
+  public function testResolverUS() {
+    // Fake CloudFlare header.
+    $request = Request::create('https://mystore.com/shop');
+    $this->container->get('request_stack')->push($request);
+
+    $manager = $this->container->get('plugin.manager.store_resolver_method');
+    $store = $manager->resolve();
+
+    $this->assertInstanceOf(StoreInterface::class, $store);
+    $this->assertEquals($store->id(), $this->store1->id());
+  }
+
+  /**
+   * Tests the resolver based on country code header.
+   */
+  public function testResolverCA() {
+    // Fake CloudFlare header.
+    $request = Request::create('https://ca.mystore.com/shop');
+    $this->container->get('request_stack')->push($request);
+
+    $manager = $this->container->get('plugin.manager.store_resolver_method');
+    $store = $manager->resolve();
+
+    $this->assertInstanceOf(StoreInterface::class, $store);
+    $this->assertEquals($store->id(), $this->store2->id());
+  }
+
+  public function testIfMissing() {
+    $manager = $this->container->get('plugin.manager.store_resolver_method');
+    $store = $manager->resolve();
+
+    $this->assertNotInstanceOf(StoreInterface::class, $store);
+  }
+
+}
